3e0c82465be8cb7df579f1424d37c34c5401d18d
psyfer <psyferblock.io@gmail.com>
2023-09-06 09:54:47 +0300

    route paths and daisyui

---
+++ src/app/components/Notification.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notification() {
+	return (
+		<div className="bg-accent h-12 text-white px-4  flex  justify-center  items-center ">
+			Notification
+		</div>
+	);
+}
+
+export default Notification;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#bedb6f",
+					secondary: "#bd56e2",
+					accent: "#f43f5e",
+					neutral: "#27313a",
+					"base-100": "#f1f5f8",
+					info: "#23c1e1",
+					success: "#44dabe",
+					warning: "#f9ae53",
+					error: "#dc2626",
+				},
+			},
+		],
+	},
+	plugins: [require("daisyui")],
+};
+export default config;
